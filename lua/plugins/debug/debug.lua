630d838644ea50a5b362c864b6dac8af2908474f
tm-mse <timothe.musete@gmail.com>
2024-11-26 17:54:49 +0100

    feat: add lazygit and golang debuger

--- lua/plugins/debug/debug.lua
+++ lua/plugins/debug/debug.lua
@@ -2,7 +2,6 @@ return {
 	{
 		'mfussenegger/nvim-dap',
 		dependencies = {
-			"theHamsta/nvim-dap-virtual-text",
 			"leoluz/nvim-dap-go",
 			"rcarriga/nvim-dap-ui",
 			"nvim-neotest/nvim-nio",
@@ -11,7 +10,6 @@ return {
 		config = function ()
 			local dap, dapui = require('dap'),require('dapui')
 			dapui.setup()
-			require('nvim-dap-virtual-text').setup()
 			require('dap-go').setup()
 
 			-- makes Break point look prettier
@@ -43,6 +41,11 @@ return {
     	{ "<leader>di", function() require("dap").step_into() end, mode = "n"},
     	{ "<leader>dO", function() require("dap").step_over() end, mode = "n"},
     	{ "<leader>do", function() require("dap").step_out() end, mode = "n"},
+    	{ "<leader>dq", function() require("dapui").close() end, mode = "n"},
+    	{ "<leader>du", function() require("dapui").open() end, mode = "n"},
+			-- run test in golang
+    	{ "<leader>gt", function() require("dap-go").debug_test() end, mode = "n"},
+    	{ "<leader>glt", function() require("dap-go").debug_last_test() end, mode = "n"},
 		},
   },
 }
--- lua/plugins/lsp/null-ls.lua
+++ lua/plugins/lsp/null-ls.lua
@@ -1,9 +1,13 @@
 return {
  "jose-elias-alvarez/null-ls.nvim",
  opts = function(_, opts)
-   local null_ls = require("null-ls")
+   local nls = require("null-ls")
    opts.sources = vim.list_extend(opts.sources or {}, {
-     null_ls.builtins.diagnostics.yamllint, -- Ajoute yamllint comme linter
+     nls.builtins.diagnostics.yamllint, -- Ajoute yamllint comme linter
+		 nls.builtins.code_actions.gomodifytags,
+     nls.builtins.code_actions.impl,
+     nls.builtins.formatting.goimports,
+     nls.builtins.formatting.gofumpt,
    })
  end,
 }
